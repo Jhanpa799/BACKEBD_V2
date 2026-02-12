ba3404ee4bf65f753f92d536591c06f6ff0c51a3
= <=>
2026-02-11 19:31:07 -0500

    Se ajusta take y skip en ProductController

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -20,8 +20,8 @@ export class ProductsController {
   @Get()
   //Se pone GetProductsDto para poder recibir parámetros. El GetProductDto
   findAll(@Query() query: GetProductsDto) {
-    const take = query.take ? query.take:100;
-    const skip = query.skip ? query.skip:20;
+    const take = query.take ? query.take:10;
+    const skip = query.skip ? query.skip:0;
     const category = query.category_id ? query.category_id : null;
     return this.productsService.findAll(category,take,skip);
   }
